2e6c3c6174abae7382f7bad5d05f8b15f91d1778
Allen Putich <allen.putich@gmail.com>
2024-05-03 06:02:08 -0700

    update wavegarden url

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 const path = require('path');
 
 const corsOptions = {
-    origin: ['https://eventbrite-promoter-dashboard.vercel.app', 'https://wavegarden-a9640094dac7.herokuapp.com/'],
+    origin: ['https://wavegarden-promoter-dashboard.vercel.app/', 'https://wavegarden-a9640094dac7.herokuapp.com/'],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
